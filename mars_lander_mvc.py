663e355bd19f9e11decc38b902836741c8660bd5
arpanrau <arpan.rau@gmail.com>
2016-10-26 13:02:41 -0400

    Whee

---
+++ mars_lander_mvc.py
@@ -0,0 +1,27 @@
+"""Framework for MVC mars lander game
+"""
+
+import pygame
+
+class Lander(object):
+"""Model of Lander, with attributes, X,Y,Rotation,Fuel,dX, and dY"""
+
+class LanderView(object):
+   """Handles display of lander"""
+
+class Gauge(object):
+   """handles display of fuel, altitude, and velocity guages"""
+
+class LanderController(object):
+   """Controls key-presses to rotate lander and fire thrusters"""
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+
+   """Main function for the code"""
+
+
+if __name__ == '__main__':
+    main()
