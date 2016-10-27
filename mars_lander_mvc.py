2c1786d288ec13e7ecc1d189fa2e024b87ed4299
arpanrau <arpan.rau@gmail.com>
2016-10-27 14:59:11 -0400

    Wheeeee

--- mars_lander_mvc.py
+++ mars_lander_mvc.py
@@ -4,7 +4,8 @@
 import pygame
 
 class Lander(object):
-"""Model of Lander, with attributes, X,Y,Rotation,Fuel,dX, and dY"""
+"""Model of Lander, with attributes, X,Y,Rotation,Fuel,dX, and dY
+   and methods gravity_update ,roll, and fire_thrusters"""
 
 class LanderView(object):
    """Handles display of lander"""
@@ -13,7 +14,8 @@ class Gauge(object):
    """handles display of fuel, altitude, and velocity guages"""
 
 class LanderController(object):
-   """Controls key-presses to rotate lander and fire thrusters"""
+   """Controls key-presses to rotate lander and fire thrusters
+      (calls lander methods)"""
 
 
 def main():
