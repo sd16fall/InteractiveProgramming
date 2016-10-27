660b6773960aa6608bbde2dc866e2d30dd9bfe29
tasandy <san.ta1194@gmail.com>
2016-10-27 16:08:58 -0400

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -1,17 +1,15 @@
 import pygame, sys
 from pygame.locals import *
 
-Score_P1 = 0
-Score_P2 = 0
 pygame.init()
 
 FPS = 30 #frames per second
 screen = pygame.display.set_mode((800, 600))
 screen.fill(0,0,0)
 pygame.display.set_caption('Tetris')
-    while Score_P1 < 3 or Score_P2 < 3: #main game loop
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit
-        pygame.display.update()
+while event == True: #main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit
+            pygame.display.update()
