cabc0dcf47bb021f8d93a1d4c33c598fcd7aea76
AndrewHolmes <andrew.t.holmes97@gmail.com>
2016-10-31 10:10:48 -0400

    Add files via upload

--- Unfolded.py
+++ Unfolded.py
@@ -1,3 +1,14 @@
+""" UNFOLDED
+RULES:
+	The player begins at the red space indicated on the grid, which represents the position of a 6 sided, 1X1 cube. Moving on the grid rotates and unfolds the cube,
+	meaning that once the player has rolled onto a face of the cube, it cannot be used again. The objective is to reach the blue space on the grid. Green 
+	spaces indicate locations that can still be moved to.
+
+For every direcion rolled on the grid, the game keeps track of the position of the face relative to every other face on the cube, and tracks which ones have already
+been used.
+
+"""
+
 import pygame
 import random
  
@@ -19,35 +30,40 @@ MARGIN = 5
  
 # Create a 2 dimensional array. A two dimensional
 # array is simply a list of lists.
-def shift(l,n):
+def shift(l,n): #Shifts a list l to the right (negative n) or left (positive n) the number of times indicated by n
 		return l[n:] + l[:n]
 
-def options(xroll, used, model, n):
-		#check if things in xroll are in used
-		#check that we arxen't along a wall
-		#apply positions and color grid accordingly
-		for i in range(n):
+def options(xroll, used, model, n): #Lights up green squares on the grid to indicate where the player can move to next
+		for i in range(n): #Returns green squares from the previous turn back to white for the next turn
 			for j in range(n):
 				if model.grid[i][j] == 2:
 					model.grid[i][j] = 0
-		for i in xroll:
-			if i not in used:
-				if i == xroll[0] and model.int_column != 0:
+		for i in xroll: #The faces in xroll always correspond to the potential moves the player can make, so we sort through the list
+			if i not in used: #Checks to see if the face the player could rotate onto has been used yet. If so, the space does not turn green
+				if i == xroll[0] and model.int_column != 0: #Sets the left space green
 					model.grid[model.int_row][model.int_column - 1] = 2
-				if i == xroll[1] and model.int_row != (n-1):
+				if i == xroll[1] and model.int_row != (n-1): #Sets the down space green
 					model.grid[model.int_row + 1][model.int_column] = 2
-				if i == xroll[2] and model.int_column != (n-1):
+				if i == xroll[2] and model.int_column != (n-1): #Sets the right space green
 					model.grid[model.int_row][model.int_column + 1] = 2
-				if i == xroll[3] and model.int_row != 0:
+				if i == xroll[3] and model.int_row != 0: #Sets the up space green
 					model.grid[model.int_row - 1][model.int_column] = 2
 
-def random_ending(model):
+def random_ending(model): #Defines a list of potential end objective locations in the form [row, column] and randomly chooses one from the list
 	objectives = [[2,1], [1,2], [1,6], [2,7], [6,1], [7,2], [7,6], [6,7]]
 	return random.choice(objectives)
 	
 
 class Grid(object):
+	""" Represents a 2D grid
+	
+	attributes: grid, int_column, int_row, xroll, yroll, used, temp
+	"""
+
+
 	def __init__(self):
+		# Create a n by n 2 dimensional array. A two dimensional
+		# array is simply a list of lists.
 		n = 9
 		self.grid = []
 		for row in range(n):
@@ -58,24 +74,22 @@ class Grid(object):
 				self.grid[row].append(0)  # Append a cell
 		self.int_column = 4
 		self.int_row = 4
-		self.grid[self.int_row][self.int_column] = 1
-		self.xroll = ['L', 'F', 'R', 'B']
-		self.yroll = ['D', 'F', 'U', 'B']
-		self.used = ['D']
+		self.grid[self.int_row][self.int_column] = 1 #Set initial position of on the grid to red
+		self.xroll = ['L', 'F', 'R', 'B'] #Sets the initial orientation of the block along the x axis (rotating the block to the left or right)
+		self.yroll = ['D', 'F', 'U', 'B'] #Sets the initial orientation of the block along the y axis (rotating the block up or down)
+		self.used = ['D'] #The block starts on this face, so it is added to the list of used faces from the very beginning. We cannot use it again.
 		self.temp = []
-		#thing = [[3, 5], [4, 6], [7, 8]]
-		#return random.choice(thing)
-
-		#end = function
-		#grid[f[0]][f[1]]
 
 class GridView(object):
+	""" Draws a 2D grid
+	"""
+
 	def __init__(self, model):
 		self.model = model
 		
 	def draw(self, screen):
 		n = 9
-		for row in range(n):
+		for row in range(n): #Set all the grid spaces to white and changes the color of a space when it is assigned a value of 1, 2, or 3
 			for column in range(n):
 				color = WHITE
 				if self.model.grid[row][column] == 1:
@@ -92,32 +106,39 @@ class GridView(object):
 								  HEIGHT])
 
 class GridController(object):
+	"""Controls movement around a 2D grid
+
+	attributes: objective
+	"""
+
 	def __init__(self, model):
 		self.model = model
-		random_ending(self.model)
-		objective = random_ending(self.model)
+		objective = random_ending(self.model) #Chooses a random position from objectives, assigns it to objective, and sets that position to blue
 		self.objective = objective
 		model.grid[objective[0]][objective[1]] = 3
 
 
 	def grid_event(self, event):
 		n = 9
-		play_again = 1
-		options(self.model.xroll, self.model.used, self.model, n)
+		options(self.model.xroll, self.model.used, self.model, n) #Lights up potential movement options before play has started
 		if self.model.int_row == self.objective[0] and self.model.int_column == self.objective[1]:
-			print 'boo yah'
-		if event.type == pygame.KEYDOWN:       
-			if event.key == pygame.K_UP and self.model.int_row != 0 and self.model.grid[self.model.int_row - 1][self.model.int_column] != 1 and self.model.grid[self.model.int_row - 1][self.model.int_column] != 0:
-				self.model.xroll[1] = self.model.yroll[0]
+			print 'You win!'
+			for row in range(n):
+				for column in range(n): #Removes green squares upon reaching objective to end gameplay
+					if self.model.grid[row][column] == 2:
+						self.model.grid[row][column] = 0
+		if event.type == pygame.KEYDOWN:      
+			if event.key == pygame.K_UP and self.model.int_row != 0 and self.model.grid[self.model.int_row - 1][self.model.int_column] != 1 \
+			and self.model.grid[self.model.int_row - 1][self.model.int_column] != 0: #Checks that that the player is not along the top wall and that the square they are moving toward is not red or white
+				self.model.xroll[1] = self.model.yroll[0] #Shifts xroll and yroll to maintain relationships between sides of the cube
 				self.model.xroll[3] = self.model.yroll[2]
 				self.model.yroll = shift(self.model.yroll, -1)
-				row = self.model.int_row - 1
+				row = self.model.int_row - 1 #Updates the grid position
 				column = self.model.int_column
 				self.model.grid[row][column] = 1
 				self.model.int_row = row
 				self.model.int_column = column
-				self.model.used.append(self.model.yroll[0])
-				options(self.model.xroll, self.model.used, self.model, n)
+				self.model.used.append(self.model.yroll[0]) #Adds the face that was rolled onto to the used list
 				
 			if event.key == pygame.K_RIGHT and self.model.int_column != (n-1) and self.model.grid[self.model.int_row][self.model.int_column + 1] != 1 \
 			and self.model.grid[self.model.int_row][self.model.int_column + 1] != 0:
@@ -130,7 +151,6 @@ class GridController(object):
 				self.model.int_row = row
 				self.model.int_column = column
 				self.model.used.append(self.model.yroll[0])
-				options(self.model.xroll, self.model.used, self.model, n)
 
 			if event.key == pygame.K_LEFT and self.model.int_column != 0 and self.model.grid[self.model.int_row][self.model.int_column - 1] != 1 \
 			and self.model.grid[self.model.int_row][self.model.int_column - 1] != 0:
@@ -143,9 +163,9 @@ class GridController(object):
 				self.model.int_row = row
 				self.model.int_column = column
 				self.model.used.append(self.model.yroll[0])
-				options(self.model.xroll, self.model.used, self.model, n)
 
-			if event.key == pygame.K_DOWN and self.model.int_row != (n-1) and self.model.grid[self.model.int_row + 1][self.model.int_column] != 1 and self.model.grid[self.model.int_row + 1][self.model.int_column] != 0:
+			if event.key == pygame.K_DOWN and self.model.int_row != (n-1) and self.model.grid[self.model.int_row + 1][self.model.int_column] != 1 \
+			and self.model.grid[self.model.int_row + 1][self.model.int_column] != 0:
 				self.model.xroll[1] = self.model.yroll[2]
 				self.model.xroll[3] = self.model.yroll[0]
 				self.model.yroll = shift(self.model.yroll, 1)
@@ -155,13 +175,12 @@ class GridController(object):
 				self.model.int_row = row
 				self.model.int_column = column
 				self.model.used.append(self.model.yroll[0])
-				options(self.model.xroll, self.model.used, self.model, n)
  
 def main(): 
 	# Initialize pygame
 	pygame.init()
 	# Set the HEIGHT and WIDTH of the screen
-	WINDOW_SIZE = [230, 300]
+	WINDOW_SIZE = [230, 230]
 	screen = pygame.display.set_mode(WINDOW_SIZE)
 	 
 	# Set title of screen
