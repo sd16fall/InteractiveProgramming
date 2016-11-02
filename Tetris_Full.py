8bd1531a97ac97d62154013cc82fec32ddae1308
awu1 <awu1@babson.edu>
2016-11-01 21:17:49 -0400

    Update Tetris_Full.py

--- Tetris_Full.py
+++ Tetris_Full.py
@@ -14,6 +14,7 @@ class Piece:
                  ((0,0,0,0,0),(0,1,0,0,0),(0,1,1,0,0),(0,0,1,0,0),(0,0,0,0,0)),
                  ((0,0,0,0,0),(0,0,1,1,0),(0,1,1,0,0),(0,0,0,0,0),(0,0,0,0,0)))
 
+
     z_block = (((0,0,0,0,0),(0,0,0,1,0),(0,0,1,1,0),(0,0,1,0,0),(0,0,0,0,0)),
                  ((0,0,0,0,0),(0,0,0,0,0),(0,1,1,0,0),(0,0,1,1,0),(0,0,0,0,0)),
                  ((0,0,0,0,0),(0,0,1,0,0),(0,1,1,0,0),(0,1,0,0,0),(0,0,0,0,0)),
@@ -49,7 +50,7 @@ class Piece:
 
     def rotater(self, clockwise = True):
         if clockwise:
-            if self.rotate == 3: #if piece is at rotation position 3, resets back to original position
+            if self.rotate == 3:
                 self.rotate = 0
             else:
                 self.rotate +=1
@@ -76,18 +77,19 @@ class Board:
         self.board_pix_height = self.board_height * self.block_size #translates the board dimension into pixels
         self.board_pix_width = self.board_width * self.block_size
         self.board = []
-        for i in range(self.board_width):
-            row = []*self.board_height
+        for i in range(self.board_height):
+            row = [0]*self.board_width
             self.board.append(row)
+        print 'board', self.board
         self.generate_piece()
 
     def generate_piece(self): #sets position on board for where piece shows up
         self.piece = Piece()
-        self.x_piece = 4
+        self.x_piece = 3
         self.y_piece = 0
 
-    def block_error(self, x, y): #checks if block is not colliding with the board
-        if x >= self.board_width: #collides with right wall
+    def collision(self, x, y): #checks if block is not colliding with the board
+        """if x >= self.board_width: #collides with right wall
             return True
         elif y >= self.board_height: #collides with bottom
             return True
@@ -95,73 +97,112 @@ class Board:
             return True
         elif self.board[y][x]: #overlaps
             return True
+        return False"""
+
+        #collides with bottom
+        bottom_of_piece = self.piece.piece_array[len(self.piece.piece_array)-2]
+        #print bottom_of_piece
+        for r in bottom_of_piece:
+            print "1"
+            if r == 1:
+                print "2"
+                print "self y", self.y_piece
+                print self.board_height
+                if self.y_piece >= 15:
+                    print "3"
+                    return True
         return False
 
-    def piece_hits_board(self, dx, dy):
-        for y in range(len(self.piece.piece_type)): #looks at the y value for the array of a piece
-            for x in range(len(self.piece.piece_type[y])): #looks at the x value for every row of the array
-                if self.piece.piece_type[y][x]:
-                    error = self.block_error(x=x+dx, y=y+dy)
+    def piece_hits_board(self, dx, dy): #checks if any square of the piece hits the board
+        for y in range(len(self.piece.piece_array)):
+            for x in range(len(self.piece.piece_array[y])):
+                if self.piece.piece_array[y][x]:
+                    #print 'x', x, 'y', y
+                    error = self.collision(x = x + dx, y = y + dy)
                     if error:
                         return error
 
     def can_move(self, dx, dy):
-        new_dx = self.x_piece + dx #takes the current position of the piece and determines whether the proposed action is acceptable
+        #print 'can x', self.x_piece
+        #print 'can y', self.y_piece
+        new_dx = self.x_piece + dx
         new_dy = self.y_piece + dy
+        #print 'new_dx', new_dx
+        #print 'new_dy', new_dy
         if self.piece_hits_board(dx = new_dx, dy = new_dy):
             return False
         return True
 
-    def attach_piece(self):
-        for y in range(len(self.piece)):
-            for x in range(len(self.piece[y])):
-                if self.piece[y][x]:
-                    self.piece[y][x] = self.board[y+self.y_piece][x+self.x_piece] #adds values of new piece joining existing piece
+    def absorb_piece(self): #stop when there is a 1 below any 1 in the piece or if it hits the board
+        for y in range(len(self.piece.piece_array)):
+            for x in range(len(self.piece.piece_array[y])):
+                if self.piece.piece_array[y][x]==1:
+                    #print 'piece', self.piece.piece_array[y][x], 'board', self.board[y+1][x]
+                    print self.piece.piece_array[len(self.piece.piece_array)-2]
+                    if self.piece.piece_array[y][x] == self.board[y+1][x] or self.collision(x,y):
+
+                        print self.piece.piece_array[y][x]
+                        print self.board[y][x]
+                        print "hi"
+                        self.board[y][x] = self.piece.piece_array[y][x]
+                        print self.board[y][x]
+
+                        #self.draw_blocks((0,0,225),offset_x = 0,offset_y = 0)
+                #self.piece.piece_array[y][x] = self.board[y + self.y_piece][x + self.x_piece]
         self.generate_piece()
 
     def rotate_piece(self, clockwise = True):
         self.piece.rotater(clockwise)
         #what do we do if the piece cannot rotate?'''
 
-    def move(self, dx, dy): #checks if the proposed action is acceptable and then moves the piece
+    def move(self, dx, dy):
         if self.can_move(dx,dy):
             self.x_piece += dx
             self.y_piece += dy
 
-    def piece_falls(self): #moves the piece drop down if it is able to, otherwise either attach the piece or delete the line
+    def piece_falls(self):
         if(self.can_move(dx = 0, dy = 1)):
             self.move(dx = 0, dy = 1)
         else:
-            self.attach_piece()
-            self.delete_lines()
+            self.absorb_piece()
+            self.delete_multiple_lines()
 
-    def hard_drop(self): #drops the piece all the way to the bottom
+    def hard_drop(self):
         while self.can_move(dx = 0, dy = 1):
             self.piece_falls()
         self.piece_falls()
 
-    def delete_line(self, y): #checks if entire row is filled and if so, deletes a line and pushes everything else down one row
-        while y>=1:
+    def delete_line(self, y):
+        """while y>=1:
             self.board[y] = list(self.board[y-1])
-            y -= 1
+            y -= 1"""
 
     def delete_multiple_lines(self):
-        if range(len(self.piece[y])) == True:
-            delete = y in range(len(self.piece)) #sets delete to the number of rows filled
+        """if range(len(self.piece.piece_array[y])) == True:
+            delete = y in range(len(self.piece))
             for y in delete:
-                self.delete_line(y)
+                self.delete_line(y)"""
+        pass
 
-    def draw_blocks(self, color=(0,0,255), offset_x = 4, offset_y = 0):
+    def draw_blocks(self, color=(0,0,255), offset_x = 3, offset_y = 0):
         for r in range(0,5):
             for c in range(0,5):
                 block = self.piece.piece_array[r][c]
                 if block == 1:
-                    x = self.block_size*r + offset_x
-                    y = self.block_size*c + offset_y
+                    x = self.block_size*(r+self.x_piece) + offset_x
+                    y = self.block_size*(c+self.y_piece) + offset_y
                     pygame.draw.rect(self.screen, (0,0,225), (x, y, self.block_size, self.block_size))
 
+    def draw_board(self, color =(0,0,225)):
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                cell = self.board[r][c]
+                if cell == 1:
+                    #print 'r', r, 'c',c
+                    pygame.draw.rect(self.screen, (0,0,225), (c, r, self.block_size, self.block_size))
+
     def game_over(self):
-        return sum(self.board[0]) > 0 or sum(self.board[1]) > 0 #if the pieces reach the top of the board then game is over
+        pass #return sum(self.board[0]) > 0 or sum(self.board[1]) > 0
 
 class Game:
 
@@ -204,7 +245,8 @@ class Game:
                 elif event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-            self.board.draw_blocks(self, offset_x = 4, offset_y = 0)
+            self.board.draw_blocks(self, offset_x = 3, offset_y = 0)
+            self.board.draw_board(self)
             pygame.display.update()
 
 if __name__ == "__main__":
