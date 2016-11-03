82d6f744b49e3dab19d4607c57103ab2eb029dec
tasandy <san.ta1194@gmail.com>
2016-11-03 16:12:09 -0400

    added in win instance

--- pokemon_pong.py
+++ pokemon_pong.py
@@ -88,8 +88,8 @@ class PongBall(pygame.sprite.Sprite):
     #         return True
     #     return False
 
-    def collision_pikas(self):
-        if pygame.sprite.spritecollide(Pikachu, PongBall, True):
+#     def collision_pikas(self):
+#         if pygame.sprite.spritecollide(Pikachu, PongBall, True):
             
 
 class AIPikachu(pygame.sprite.Sprite):
@@ -123,63 +123,79 @@ def AI_score():
     global AI_wins
     AI_wins += 1
     print 'AI wins', AI_wins
-    
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((window_width, window_height))
+
+def win_game():
+    global p_wins, AI_wins
+    if p_wins == 5:
+        p_wins = 0
+        AI_wins = 0
+        #p_wins_text = text.render('You win!', 1, (255,0,0))
+        reset_game()
+    elif AI_wins == 5:
+        p_wins = 0
+        AI_wins = 0
+        #AI_wins_text = text.render('You lose!',1 (255,0,0))
+        reset_game()
+
+def reset_game():
+    global pika
+    pika.pikac.centery = window_height / 2.0
+
+'''main'''
+pygame.init()
+screen = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('Pokemon Pong')
+pika = Pikachu(0,0)
+pongball = PongBall()
+pika2 = AIPikachu()
+running = True
+while running == True:
+    pika.move_pika(pygame.key.get_pressed())
+    pika2.move_AI()
+    pongball.set_ball()
+    pongball.collision_wall()
+    pongball.collision(pika)
+    # pongball.collision_pikas()
+
+    """if pygame.sprite.collide_rect(pika, pongball):
+        pongball.ball.centery -= 50
+        pongball.ball.centerx -= 0
+        pongball.ball.move_ip(-15,-10)"""
+
+    #pongball.collision_pad()
+    #pongball.score()
+    # pongball.random_move()
+    # '''moving Pikachu'''
+    # if 150 < pikachu.pika.bottom < window_height:
+    #     if key[pygame.K_DOWN]:
+    #         pikachu.pika.centery += 15
+    #     elif key[pygame.K_UP]:
+    #         pikachu.pika.centery -= 15
+    # elif pikachu.pika.bottom >= window_height:
+    #     pikachu.pika.centery -= 15
+    # elif pikachu.pika.bottom <= 150:
+    #     pikachu.pika.centery += 15
+    '''text'''
     text = pygame.font.SysFont("Arial", 60)
-    pygame.display.set_caption('Pokemon Pong')
-    pygame.display.set_caption('Pokemon Pong')
-    pika = Pikachu(0,0)
-    pongball = PongBall()
-    pika2 = AIPikachu()
-    running = True
-    while running == True:
-        pika.move_pika(pygame.key.get_pressed())
-        pika2.move_AI()
-        pongball.set_ball()
-        pongball.collision_wall()
-        pongball.collision_pikas()
+    win_text = pygame.font.SysFont("Arial", 200)
+    p_render = text.render(str(p_wins), 1, (255,0,0))
+    AI_render = text.render(str(AI_wins), 1, (255,0,0))
+    '''image stuff'''
+    background = pygame.image.load('grass.png')
+    background = pygame.transform.smoothscale(background, (window_width, window_height))
+    screen.blit(background, (0,0))
+    screen.blit(pika.image, pika.rect)
+    screen.blit(pongball.image, pongball.rect)
+    screen.blit(pika2.image, pika2.rect)
+    screen.blit(p_render, (50,10))
+    screen.blit(AI_render, (1100,10))
+    #screen.blit(p_wins_text, (600,600))
+    #screen.blit(AI_wins_text, (600,600))
+    pygame.display.flip()
+    pygame.display.update()
+    '''quit event'''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
 
-        """if pygame.sprite.collide_rect(pika, pongball):
-            pongball.ball.centery -= 50
-            pongball.ball.centerx -= 0
-            pongball.ball.move_ip(-15,-10)"""
-
-        #pongball.collision_pad()
-        #pongball.score()
-        # pongball.random_move()
-        # '''moving Pikachu'''
-        # if 150 < pikachu.pika.bottom < window_height:
-        #     if key[pygame.K_DOWN]:
-        #         pikachu.pika.centery += 15
-        #     elif key[pygame.K_UP]:
-        #         pikachu.pika.centery -= 15
-        # elif pikachu.pika.bottom >= window_height:
-        #     pikachu.pika.centery -= 15
-        # elif pikachu.pika.bottom <= 150:
-        #     pikachu.pika.centery += 15
-        '''text'''
-        p_render = text.render(str(p_wins), 1, (255,0,0))
-        AI_render = text.render(str(AI_wins),1, (255,0,0))
-        
-        '''image stuff'''
-        background = pygame.image.load('grass.png')
-        background = pygame.transform.smoothscale(background, (window_width, window_height))
-        screen.blit(background, (0,0))
-        screen.blit(pika.image, pika.rect)
-        screen.blit(pongball.image, pongball.rect)
-        screen.blit(pika2.image, pika2.rect)
-        screen.blit(p_render, (50,10))
-        screen.blit(AI_render, (1100,10))
-        pygame.display.flip()
-        pygame.display.update()
-        
-        '''quit event'''
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-
-if __name__ == "__main__":
-    main()
